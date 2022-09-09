3df53bd575f7cf5eeecadc36f2df42b494eac061
sebastiansouza <sebastian.asouzasp@gmail.com>
2022-09-09 20:38:20 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -14,6 +17,13 @@ function SalesCard() {
     
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
+
+    useEffect(() => {
+        axios.get("http://localhost:8080/sales")
+        .then(response => {
+            console.log(response.data);
+        })
+    },[]);
 
 
     return (

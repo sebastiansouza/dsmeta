c5ee47c9cdb46722d86b03536a41446e255ab9fc
sebastiansouza <sebastian.asouzasp@gmail.com>
2022-09-09 21:02:27 -0300

    Sale List

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -4,6 +4,8 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Sale } from "../../models/sale";
+import { BASE_URL } from "../../utils/request";
 
 import NotificationButton from '../NotificationButton';
 import './styles.css';
@@ -12,18 +14,20 @@ import './styles.css';
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365))
-    const max = new Date ()
+    const max = new Date()
 
-    
+
     const [minDate, setMinDate] = useState(min)
     const [maxDate, setMaxDate] = useState(max)
 
+    const [sales, setSales] = useState<Sale[]>([]);
+
     useEffect(() => {
-        axios.get("http://localhost:8080/sales")
-        .then(response => {
-            console.log(response.data);
-        })
-    },[]);
+        axios.get(`${BASE_URL}/sales`)
+            .then(response => {
+                setSales(response.data.content);
+            })
+    }, []);
 
 
     return (
@@ -34,7 +38,7 @@ function SalesCard() {
                     <div className="dsmeta-form-control-container">
                         <DatePicker
                             selected={minDate}
-                            onChange={(date: Date) => setMinDate (date)}
+                            onChange={(date: Date) => setMinDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
@@ -42,7 +46,7 @@ function SalesCard() {
                     <div className="dsmeta-form-control-container">
                         <DatePicker
                             selected={maxDate}
-                            onChange={(date: Date) => setMaxDate (date)}
+                            onChange={(date: Date) => setMaxDate(date)}
                             className="dsmeta-form-control"
                             dateFormat="dd/MM/yyyy"
                         />
@@ -64,49 +68,27 @@ function SalesCard() {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td className="show992">#341</td>
-                                <td className="show576">10/07/2001</td>
-                                <td>Ana Cristina</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dmsmeta-red-btn-container">
-                                        <NotificationButton />
+                            {
+                                sales.map(sale => {
+                                    return (<tr key={sale.id}>
+                                        <td className="show992">{sale.id}</td>
+                                        <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                        <td>{sale.sellerName}</td>
+                                        <td className="show992">{sale.visited}</td>
+                                        <td className="show992">{sale.deals}</td>
+                                        <td>R$ {sale.amount.toFixed(2)}</td>
+                                        <td>
+                                            <div className="dmsmeta-red-btn-container">
+                                                <NotificationButton />
 
-                                    </div>
-                                </td>
+                                            </div>
+                                        </td>
 
-                            </tr>
-                            <tr>
-                                <td className="show992">#41</td>
-                                <td className="show576">24/12/1986</td>
-                                <td>Ariane</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dmsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
+                                    </tr>
 
-                            </tr>
-
-                            <tr>
-                                <td className="show992">#33</td>
-                                <td className="show576">02/12/1996</td>
-                                <td>Aline</td>
-                                <td className="show992">15</td>
-                                <td className="show992">11</td>
-                                <td>R$ 55300.00</td>
-                                <td>
-                                    <div className="dmsmeta-red-btn-container">
-                                        <NotificationButton />
-                                    </div>
-                                </td>
-                            </tr>
+                                    )
+                                })
+                            }
 
                         </tbody>
                     </table>
---
+++ frontend/src/models/sale.ts
@@ -0,0 +1,15 @@
+export type Sale = {
+
+
+    id: number;
+    sellerName: string;
+    date: string;
+    visited: number;
+    deals: number;
+    amount: number;
+
+
+
+}
+
+
